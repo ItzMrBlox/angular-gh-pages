24c33a2c5a6422b9fc2e2026c0c2b4eccaa50b4c
Blox <itzmrblox@gmail.com>
2023-02-25 19:59:25 +0100

    asd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app.routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { CharactersComponent } from './characters/characters.component';
 
 @NgModule({
 	declarations: [
 		AppComponent,
-  HomeComponent
+  HomeComponent,
+  CharactersComponent
 	],
 	imports: [
 		BrowserModule,
